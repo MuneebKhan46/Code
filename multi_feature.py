23013ee3e3ddcc43e68c0af3b2cff96500804d2b
MuneebKhan46 <81796045+MuneebKhan46@users.noreply.github.com>
2024-07-22 00:36:13 +0900

    Update multi_feature.py

--- multi_feature.py
+++ multi_feature.py
@@ -356,7 +356,7 @@ cnn_wcw_model.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accur
     
 wcw_model_checkpoint = keras.callbacks.ModelCheckpoint(filepath='/Code/Models/CNN_MultiFeature_wCW_SIGMOID.keras', save_best_only=True, monitor='val_accuracy', mode='max', verbose=1 )
 wcw_model_early_stopping = keras.callbacks.EarlyStopping(monitor='val_accuracy', min_delta=0, patience=10, restore_best_weights=True)
-wcw_history = cnn_wcw_model.fit(X_train, y_train, epochs=50, batch_size=8, validation_data=(X_val, y_val), callbacks=[wcw_model_checkpoint, wcw_model_early_stopping])
+wcw_history = cnn_wcw_model.fit(X_train, y_train, epochs=50, validation_data=(X_val, y_val), callbacks=[wcw_model_checkpoint, wcw_model_early_stopping])
 
 #########################################################################################################################################################################################################################################
 #########################################################################################################################################################################################################################################
