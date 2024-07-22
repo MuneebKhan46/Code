2d02ebc55ec3e3b09884f1eb7df0ed801de60cbb
MuneebKhan46 <81796045+MuneebKhan46@users.noreply.github.com>
2024-07-22 13:09:34 +0900

    Update CNN_positive.py

--- CNN_positive.py
+++ CNN_positive.py
@@ -589,7 +589,8 @@ weighted_recall *= 100
 
 
 model_name = "CNN"
-feature_name = "Normalized Difference Map"technique = "Class Balance"
+feature_name = "Normalized Difference Map"
+technique = "Class Balance"
 save_metric_details(model_name, technique, feature_name, test_acc, weighted_precision, weighted_recall, weighted_f1_score, test_loss, accuracy_0, accuracy_1, result_file_path)
 print(f"Accuracy: {test_acc:.4f} | precision: {weighted_precision:.4f}, Recall={weighted_recall:.4f}, F1-score={weighted_f1_score:.4f}, Loss={test_loss:.4f}, N.G.A Accuracy={accuracy_0:.4f}, G.A Accuracy={accuracy_1:.4f}")
 
