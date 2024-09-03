8f33878af7871a4b1127814b685a9843837f7406
MuneebKhan46 <81796045+MuneebKhan46@users.noreply.github.com>
2024-09-03 23:43:27 +0900

    Update Block_CNN.py

--- Block_CNN.py
+++ Block_CNN.py
@@ -20,7 +20,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "CPU")
 
 IMAGE_SIZE = 224
 PATCH_SIZE = 224
-BATCH_SIZE = 16
+BATCH_SIZE = 24
 LEARNING_RATE = 1e-3
 weight_decay = 1e-4
 EPOCHS = 50
