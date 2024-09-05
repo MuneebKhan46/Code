dd2c00560e6895db2ace2f45df038ce2a2a70bdc
MuneebKhan46 <81796045+MuneebKhan46@users.noreply.github.com>
2024-09-05 18:52:19 +0900

    Update base_metric.py

--- base_metric.py
+++ base_metric.py
@@ -72,27 +72,31 @@ val_data, test_data = train_test_split(temp_data, test_size=0.5, random_state=42
 
 train_loader = DataLoader(train_data, batch_size=16, shuffle=True)
 val_loader = DataLoader(val_data, batch_size=16, shuffle=False)
-test_loader = DataLoader(test_data, batch_size=16, shuffle=False)
+test_loader = DataLoader(test_data, shuffle=False)
+
+print(len(train_loader))
+print(len(val_loader))
 print(len(test_loader))
 
 psnr_scores, ssim_scores = [], []
 
 with torch.no_grad():
   for inputs, targets in test_loader:
-    inputs, targets = inputs.to(device), targets.to(device)
-    inputs = inputs.cpu().numpy()
-    targets = targets.cpu().numpy()
-    print(len(inputs))
-    for i in range(len(inputs)):
-      psnr_scores.append(psnr(targets[i], outputs[I]))
-      patch_size = min(outputs[i].shape[0], outputs[i].shape[1])
-      win_size = min(7, patch_size) 
-      if win_size >= 3:
-        ssim_val = ssim(targets[i], outputs[i], win_size=win_size, channel_axis=-1, data_range=1.0)
-        ssim_scores.append(ssim_val)
-      else:
-        print(f"Skipping SSIM for patch {i} due to insufficient size")
-
+      inputs, targets = inputs.to(device), targets.to(device)
+      inputs = inputs.cpu().numpy()
+      targets = targets.cpu().numpy()
+      
+      print(len(inputs))
+      for i in range(len(inputs)):
+          psnr_scores.append(psnr(targets[i], inputs[I]))
+          patch_size = min(inputs[i].shape[0], inputs[i].shape[1])
+          win_size = min(7, patch_size)
+          if win_size >= 3:
+              ssim_val = ssim(targets[i], inputs[i], win_size=win_size, channel_axis=-1, data_range=1.0)
+              ssim_scores.append(ssim_val)
+          else:
+              print(f"Skipping SSIM for patch {i} due to insufficient size")
+          
 avg_psnr = np.mean(psnr_scores)
 avg_ssim = np.mean(ssim_scores) if ssim_scores else 0
 
